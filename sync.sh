ec7715aab37c21fc1a7f1063f9094ab4ad0b522b
tvinko <tv@algonia.net>
2021-03-20 09:53:43 +0100

    MacOS sync  fix

--- sync.sh
+++ sync.sh
@@ -1,18 +1,18 @@
-if [ ! -d algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia ] ; then
-  mkdir algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia
+if [ ! -d /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia ] ; then
+  mkdir /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia
 fi
 
-if [ ! -d algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia/cs ] ; then
-  mkdir algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia/cs
+if [ ! -d /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia/cs ] ; then
+  mkdir /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia/cs
 fi
 
 # netcore algos sync
-cp -r algos/netcore/StockPricesTest/bin/Debug/netcoreapp3.1/*.* algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia/cs
+cp -r algos/netcore/StockPricesTest/bin/Debug/netcoreapp3.1/*.* /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia/cs
 
 # python algos sync
-cp -r algos/py algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia
+cp -r algos/py /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia
 
 # html algos sync
-cp -r algos/html algonia-bundle-macos/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia
+cp -r algos/html /Applications/algonia-designer.app/Contents/Resources/app/AlgoniaEngine/libs/algonia
 
 
